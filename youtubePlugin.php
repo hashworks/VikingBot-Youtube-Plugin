4a0dd5f9fb852d6c5d52021c89e9306661c56ab5
hashworks <admin@hashworks.net>
2014-12-28 22:29:20 +0100

    Disable plugin if allow_url_fopen can't be enabled

--- youtubePlugin.php
+++ youtubePlugin.php
@@ -2,6 +2,27 @@
 
 class youtubePlugin extends basePlugin {
 
+	private $disabled;
+
+	/**
+	 * Called when plugins are loaded
+	 *
+	 * @param mixed[]	$config
+	 * @param resource 	$socket
+	**/
+	public function __construct($config, $socket) {
+		parent::__construct($config, $socket);
+		$this->disabled = false;
+		if (!ini_get('allow_url_fopen')) {
+			try {
+				ini_set('allow_url_fopen', '1');
+			} catch (Exception $e) {
+				logMsg("Unable to enable allow_url_fopen, disabling youtubePlugin.");
+				$this->disabled = true;
+			}
+		}
+	}
+
 	/**
 	 * Called when messages are posted on the channel
 	 * the bot are in, or when somebody talks to it
@@ -11,6 +32,9 @@ class youtubePlugin extends basePlugin {
 	 * @param string $msg
 	 */
 	public function onMessage($from, $channel, $msg) {
+		if ($this->disabled === true) {
+			return;
+		}
 		preg_match_all("/http[s]{0,1}:\/\/(?:www\.){0,1}youtu(?:\.be\/[-a-z0-9]+|be\.com\/watch[?&=-a-z0-9]+)/i", $msg, $matches);
 		$matches = array_splice($matches[0], 0, 5);
 		foreach ($matches as $link) {
